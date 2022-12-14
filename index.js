c46dee2e37ef754530c3440c1d066c050634f021
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-14 17:04:30 -0500

    commit tree f449bc8c075a7e57a551632184c0f1f7c81825a8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = 'BOB'
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = ''
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 1
+}
